a937bb9e653b01ed155bf105d699c69cb1649d13
WilliamBZA <william.brander@gmail.com>
2025-02-22 18:55:58 +0200

    Add send messages sample

---
+++ exercises/x.SendMessages/Program.cs
@@ -0,0 +1,121 @@
+using nanoFramework.Networking;
+using nanoFramework.WebServer;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Security;
+using System.Threading;
+using System.IO;
+using System.Text;
+using Amqp;
+
+namespace _0.SendMessages
+{
+    public class Program
+    {
+        private static string MySsid = "dropitlikeaSquat";
+        private static string MyPassword = "DaisyToddAndButt";
+        private const string DirectoryPath = "I:\\wwwroot\\";
+
+        public static void Main()
+        {
+            Debug.WriteLine("Attempting to connect to network with previously saved credentials...");
+
+            bool success;
+            CancellationTokenSource cs = new(60000);
+            success = WifiNetworkHelper.Reconnect(requiresDateTime: true, token: cs.Token);
+
+            if (!success)
+            {
+                Debug.WriteLine("Couldn't connect with previously saved credentials. Attempting to connect with SSID and password");
+                success = WifiNetworkHelper.ConnectDhcp(MySsid, MyPassword, requiresDateTime: true, token: cs.Token);
+
+                if (!success)
+                {
+                    Debug.WriteLine($"Can't get a proper IP address and DateTime, error: {NetworkHelper.Status}.");
+                    if (NetworkHelper.HelperException != null)
+                    {
+                        Debug.WriteLine($"Exception: {NetworkHelper.HelperException}");
+                    }
+                    return;
+                }
+            }
+
+            var ipaddres = IPGlobalProperties.GetIPAddress();
+            Debug.WriteLine($"Connected with IP Address: {ipaddres.ToString()}");
+
+            var connection = new Connection(new Address("amqps://RootManageSharedAccessKey:qKXZgqfP3MS6JLAIm2IETr%2BergKTYJDwO%2BASbBjbfWQ%3D@sbroutingtest.servicebus.windows.net:5671/?verify=verify_none"));
+
+            using (WebServer server = new WebServer(80, HttpProtocol.Http, new Type[] { typeof(HomeController) }))
+            using (WebServer httpsServer = new WebServer(443, HttpProtocol.Https, new Type[] { typeof(HomeController) }))
+            {
+                server.CommandReceived += ServerCommandReceived;
+                server.Start();
+
+                var session = new Session(connection);
+
+                var receiver = new ReceiverLink(session, "Incoming", "q1");
+                receiver.Start(5, onMessage);
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+
+        private static void onMessage(IReceiverLink receiver, Message message)
+        {
+            string body = null;
+            if (message.Body is byte[])
+            {
+                var bodyBytes = (byte[])message.Body;
+                body = Encoding.UTF8.GetString(bodyBytes, 0, bodyBytes.Length);
+            }
+            else if (message.Body is string)
+            {
+                body = (string)message.Body;
+            }
+            Debug.WriteLine($"{DateTime.UtcNow} Received message: '{body}'");
+
+            try
+            {
+                receiver.Accept(message);
+            }
+            catch (Exception) { }
+        }
+
+        private static void ServerCommandReceived(object source, WebServerEventArgs e)
+        {
+            try
+            {
+                var url = e.Context.Request.RawUrl;
+                Debug.WriteLine($"Command received: {url}, Method: {e.Context.Request.HttpMethod}");
+
+                var fileName = url.Substring(1);
+                if (string.IsNullOrEmpty(fileName))
+                {
+                    fileName = "index.html";
+                }
+                else
+                {
+                    var stringBuilder = new StringBuilder(fileName);
+                    stringBuilder.Replace("/", "\\");
+
+                    fileName = stringBuilder.ToString();
+                }
+
+                Debug.WriteLine($"Request file: '{fileName}'");
+
+                WebServer.SendFileOverHTTP(e.Context.Response, DirectoryPath + fileName);
+                return;
+            }
+            catch (IOException)
+            {
+                WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.NotFound);
+            }
+            catch (Exception)
+            {
+                WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
